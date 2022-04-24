a6c0adbf37858484799d7e82e2225fe9ba2b6f6e
goyo <goyito_powa@hotmail.com>
2022-04-24 21:05:30 +0200

    commit del ejercicio

---
+++ src/main/java/Cuentas/Main.java
@@ -0,0 +1,36 @@
+package Cuentas;
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = operativa_cuenta(5000);
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    public static CCuenta operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        return cuenta1;
+    }
+}
+
+    
+
